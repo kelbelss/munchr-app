3e155946fb253802a5eec505458185eee83c8e89
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-12-28 17:41:07 +0200

    Logo and Favicon

--- munchr/components/EditScreenInfo.tsx
+++ munchr/components/EditScreenInfo.tsx
@@ -10,13 +10,13 @@ export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        <Text
+        <MonoText
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)"
         >
           Open up the code for this screen:
-        </Text>
+        </MonoText>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
@@ -41,10 +41,10 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.helpLink}
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
         >
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+          <MonoText style={styles.helpLinkText} lightColor={Colors.light.tint}>
             Tap here if your app doesn't automatically update after making
             changes
-          </Text>
+          </MonoText>
         </ExternalLink>
       </View>
     </View>
