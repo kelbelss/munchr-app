0eca590b82bd27e2f1025f0bfc4e6530c775f9c0
kelbelss <124790103+kelbelss@users.noreply.github.com>
2023-12-29 16:13:13 +0200

    Sign In Page

---
+++ munchr/app/index.tsx
@@ -0,0 +1,87 @@
+import { Alert, View, Button, TextInput, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import Spinner from 'react-native-loading-spinner-overlay';
+import { supabase } from '../config/supabase';
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+
+    // sign in with email and password
+    const onSignInPress = async () => {
+        setLoading(true);
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+    };
+
+    // Create a new user
+    const onSignUpPress = async () => {
+        setLoading(true);
+        const { error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+    };
+
+
+    return (
+        <View style={styles.container}>
+            <Spinner visible={loading} />
+
+            <Text style={styles.header}>Munchr</Text>
+
+            <TextInput autoCapitalize="none" placeholder="john@doe.com" value={email} onChangeText={setEmail} style={styles.inputField} />
+            <TextInput placeholder="password" value={password} onChangeText={setPassword} secureTextEntry style={styles.inputField} />
+
+            <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+                <Text style={{ color: '#363232' }}>Sign in</Text>
+            </TouchableOpacity>
+            <Button onPress={onSignUpPress} title="Create Account" color={'#363232'}></Button>
+        </View >
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 50,
+        padding: 20,
+        backgroundColor: "#EDF0F6",
+    },
+    header: {
+        fontSize: 60,
+        textAlign: "center",
+        margin: 50,
+        color: "#E6DBC8",
+    },
+    inputField: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: "#E6DBC8",
+        borderRadius: 4,
+        padding: 10,
+        color: "#363232",
+        backgroundColor: "#F3F2F0",
+    },
+    button: {
+        marginVertical: 15,
+        alignItems: "center",
+        backgroundColor: "#E6DBC8",
+        padding: 12,
+        borderRadius: 4,
+    },
+});
+
+export default Login;
